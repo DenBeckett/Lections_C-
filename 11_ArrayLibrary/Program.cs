7aed922a5d4cf9732fd696320baeb8da01c04a4d
Den Beckett <den_local@debian>
2022-11-22 22:06:43 +0300

    fix 11_ArrayLibrary

--- 11_ArrayLibrary/Program.cs
+++ 11_ArrayLibrary/Program.cs
@@ -22,7 +22,26 @@ void PrintArray(int[] col)
     Console.WriteLine();
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int index = collection.Length;
+    int count = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+             position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
