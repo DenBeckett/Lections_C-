55bd7c65d819b49e126b47f85caba986c3374656
Den Beckett <den_local@debian>
2022-11-22 21:21:54 +0300

    08_IntroMethod

---
+++ 08_IntroMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if(arg2> result) result = arg2;
+if(arg3> result) result = arg3;
+return result;
+}
+
+int a1 = 34;
+int b1 = 64;
+int c1 = 1467;
+int a2 = 147;
+int b2 = 389;
+int c2 = 3;
+int a3 = 847;
+int b3 = 78;
+int c3 = 19;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+Max(a1, b1, c1),
+Max(a2, b2, c2),
+Max(a3, b3, c3));
+
+Console.WriteLine(max);
